b78724e2747e7b11d15b34dade4b589451cda4c9
seeb03 <ben.armeni.aa@gmail.com>
2026-02-15 16:59:00 +0100

    Animazione walk/idle funzionante

--- Assets/_Project/Scripts/PlayerMovement.cs
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-
     private Rigidbody2D rb;
     private Animator animator;
     private Vector2 moveInput;
@@ -22,16 +21,18 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        // Movimento orizzontale
         rb.linearVelocity = new Vector2(moveInput.x * speed, rb.linearVelocity.y);
 
-        // Animazione
-        bool isMoving = moveInput.x != 0;
+        // Aggiorna animazione
+        bool isMoving = Mathf.Abs(moveInput.x) > 0.01f;
         animator.SetBool("isMoving", isMoving);
 
-        // Flip
-        if (moveInput.x > 0)
-            transform.localScale = new Vector3(1, 1, 1);
-        else if (moveInput.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
+        // Flip personaggio
+        if (moveInput.x > 0.01f) transform.localScale = new Vector3(1, 1, 1);
+        else if (moveInput.x < -0.01f) transform.localScale = new Vector3(-1, 1, 1);
+
+        // Debug
+        Debug.Log("MoveInput X: " + moveInput.x + " | isMoving: " + animator.GetBool("isMoving"));
     }
 }
